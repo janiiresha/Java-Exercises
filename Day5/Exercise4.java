f8be70dd35e8b08ffec49ecf8c83f565e58a1498
janiiresha <44993229+janiiresha@users.noreply.github.com>
2019-09-13 14:09:44 +0900

    Day5

---
+++ Day5/Exercise4.java
@@ -0,0 +1,13 @@
+public class Exercise4{
+    public static void main(String[]args){
+        //convert hour to seconds
+        int hourInSeconds = 15*3600;
+        //54000
+        int minutesInSeconds = 20*60;
+        //1200
+        System.out.println(hourInSeconds+minutesInSeconds);
+    }
+}    
+        
+        
+    
---
+++ Day5/Exercise5.java
@@ -0,0 +1,21 @@
+public class Exercise5{
+    public static void main(String[]args){
+        
+        int secondsPerDay = 3600;
+        int hoursOfDay = 24;
+        int daysOfYear = 365;
+        int secondsPerYear = (secondsPerDay*hoursOfDay*daysOfYear);
+        
+        System.out.println("There are 31536000 in a year");
+    
+    }    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
